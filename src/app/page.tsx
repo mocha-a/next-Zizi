6218c6057cfe1b08255a7a46b033d4a48df40388
anji <anji64852563@gmail.com>
2025-10-24 17:34:37 +0900

    2025-10-24: 1차 통합

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,6 @@ import '../styles/home/home.scss';
 
 export default function Home() {
   return (
-<<<<<<< HEAD
     <>
       <div className='header'>Zizi !</div>
       <div className="home-container">
@@ -19,16 +18,5 @@ export default function Home() {
         <Chart/>
       </div>
     </>
-=======
-  <>
-    <div className='header'>Zizi !</div>
-    <div className="home-container">
-      <Dashboard/>
-      <Mood/>
-      <NewRelease/>
-      <Chart/>
-    </div>
-  </>
->>>>>>> origin/jihyeon
   );
 }
