86929794067c9672b8067035de3e67cde23adbfd
anji <anji64852563@gmail.com>
2026-08-01 17:57:51 +0900

    feat: 비밀번호 찾기 질문 UI 추가

--- src/components/icons/Drop.tsx
+++ src/components/icons/Drop.tsx
@@ -1,16 +1,10 @@
-import Image from 'next/image';
-
 function Drop({ className }: { className?: string }) {
   return (
-    <div className={ `${className} drop` }>
-    <Image
-        src='/icons/drop.svg'
-        alt='더보기'
-        width={9}
-        height={7}
-    />
-    </div>
-  )
+    <svg width="9" height="7" viewBox="0 0 9 7" fill="none" xmlns="http://www.w3.org/2000/svg" className={`${className} drop`}>
+      <path d="M8 0.999993L4.52479 5.48608L1 0.999993" fill="#1A1A1A"/>
+      <path d="M8 0.999993L4.52479 5.48608L1 0.999993H8Z" stroke="#1A1A1A" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  );
 }
 
-export default Drop
+export default Drop;
